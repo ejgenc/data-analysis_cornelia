c61e6fbf701d9edca0262cf2511f9e9d09e3a837
ejgenc@gmail.com <ejgenc@gmail.com>
2022-01-06 17:37:01 +0100

    Create 'dodo.py', a file used by the doit package for script orchestration.

---
+++ dodo.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+
+def show_cmd(task):
+    return "executing... %s" % task.name
+
+def task_clean_main():
+    action_path = Path("src/data-cleaning/clean-main.py")
+    return {
+        "file_dep": [Path("data/main/cornelia-raw.csv")],
+        "actions": ["python {}".format(action_path)],
+        # "targets": [Path("data/cleaned/cornelia-cleaned.csv")],
+        "title": show_cmd
+    }
